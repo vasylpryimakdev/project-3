0c85db400cbbe6b8183b03e1bd885f580526d2c5
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-04-24 18:09:14 +0300

    feat(backend): initialize Express server with MongoDB connection

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,23 @@
-console.log("Welcome to Indian Coders Mern Stack Travel Diaries Application");
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const start = async () => {
+  try {
+    await mongoose
+      .connect(process.env.MONGO_URI)
+      .then(() => console.log("MongoDB is Conneted"));
+
+    app.listen(5000, () =>
+      console.log("Server is listening on port: http://localhost:5000"),
+    );
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+start();
