d6ec91b5e6dd6dd174c9ab52be68ae622b05b450
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-04-24 21:29:24 +0300

    feat(posts): add API helper for updating diary posts

--- frontend/src/api-helpers/helpers.js
+++ frontend/src/api-helpers/helpers.js
@@ -57,3 +57,21 @@ export const getPostDetails = async (id) => {
   const resData = await res.data;
   return resData;
 };
+
+export const postUpdate = async (data, id) => {
+  const res = await axios
+    .put(`/posts/${id}`, {
+      title: data.title,
+      description: data.description,
+      location: data.location,
+      image: data.imageUrl,
+    })
+    .catch((err) => console.log(err));
+
+  if (res.status !== 200) {
+    return console.log("Unable to udpate");
+  }
+
+  const resData = await res.data;
+  return resData;
+};
