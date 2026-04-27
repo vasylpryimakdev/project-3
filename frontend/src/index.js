acefdb0f8ac0c841bc04443f21ffa224439db722
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-04-27 12:53:39 +0300

    Fixed env variable name

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 import axios from "axios";
 import { store } from "./store";
 
-const BASE_URL = process.env.API_URL;
+const BASE_URL = process.env.REACT_APP_API_URL;
 
 axios.defaults.baseURL = BASE_URL;
 
